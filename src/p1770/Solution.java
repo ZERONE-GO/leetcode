278e2062c182a7caf6d4582698362ab812564c53
Zeng Zhirong <zengzhirong87@163.com>
2021-03-18 17:55:30 +0800

    1770

--- src/p1770/Solution.java
+++ src/p1770/Solution.java
@@ -5,8 +5,8 @@ import java.util.Arrays;
 public class Solution {
 
     public int maximumScore(int[] nums, int[] multipliers) {
-        int[][] backup = new int[nums.length][nums.length];
-        for(int i =0; i < nums.length; i ++) {
+        int[][] backup = new int[multipliers.length + 1][multipliers.length + 1];
+        for(int i =0; i < multipliers.length+1; i ++) {
             Arrays.fill(backup[i], -1);
         }
         return score(nums, multipliers, backup, 0, nums.length-1, 0);
@@ -17,11 +17,11 @@ public class Solution {
             return 0;
         }
         
-        if(backup[left][right] == -1) {
+        if(backup[left][nums.length - right] == -1) {
             int ls = score(nums, multipliers, backup, left + 1, right, index + 1) + nums[left] * multipliers[index];
             int rs = score(nums, multipliers, backup, left, right - 1, index + 1) + nums[right] * multipliers[index];
-            backup[left][right] =  ls > rs ? ls : rs;
+            backup[left][nums.length - right] =  ls > rs ? ls : rs;
         }
-        return backup[left][right];
+        return backup[left][nums.length - right];
     }
 }
