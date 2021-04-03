1dc4876cf7d8b37c454075f51e5f8f7e021d69ea
zengzhirong87@163.com <zengzhirong87@163.com>
2021-04-03 22:54:20 +0800

    5705

---
+++ src/p5705/Solution.java
@@ -0,0 +1,11 @@
+package p5705;
+
+public class Solution {
+
+    public boolean squareIsWhite(String coordinates) {
+        int x = coordinates.charAt(0) - 'a' + 1;
+        int y = Integer.valueOf(coordinates.substring(1));
+        
+        return ((x&1) == 1 && (y&1) == 1) ||((x&1) ==0 &&(y&1) == 0); 
+    }
+}
