9750e27eb22341138b6dd6c0b22a0d455be564d6
Zeron <zengzhirong87@163.com>
2021-04-23 16:46:29 +0800

    fix

--- src/p1402/Solution.java
+++ src/p1402/Solution.java
@@ -8,7 +8,7 @@ public class Solution {
         int[][] map = new int[satisfaction.length + 1][satisfaction.length + 1];
 
         Arrays.sort(satisfaction);
-        int max = Integer.MIN_VALUE;
+        int max = 0;
         for (int i = 1; i <= satisfaction.length; i++) {
             for (int j = 1; j <= i; j++) {
                 map[i][j] = map[i - 1][j - 1] + satisfaction[i - 1] * j;
