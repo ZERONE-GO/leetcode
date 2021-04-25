517e3e9f7b9bb2d3c7fd72343780721750b9fc93
Zeron <zengzhirong87@163.com>
2021-04-25 16:21:57 +0800

    5738

---
+++ src/p5738/Solution.java
@@ -0,0 +1,16 @@
+package p5738;
+
+public class Solution {
+    
+    public int sumBase(int n, int k) {
+        int res = 0;
+        
+        while(n > 0) {
+            res += n % k;
+            n = n / k;
+        }
+        
+        return res;
+    }
+
+}
