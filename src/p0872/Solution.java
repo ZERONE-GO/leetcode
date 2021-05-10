ef23f057de7bb1341a30fb77d9032fbd440c5d31
Zeron <zengzhirong87@163.com>
2021-05-10 16:53:16 +0800

    872

---
+++ src/p0872/Solution.java
@@ -0,0 +1,56 @@
+package p0872;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public boolean leafSimilar(TreeNode root1, TreeNode root2) {
+        List<Integer> leaves1 = new ArrayList<Integer>();
+        List<Integer> leaves2 = new ArrayList<Integer>();
+
+        preOrder(root1, leaves1);
+        preOrder(root2, leaves2);
+
+        if (leaves1.size() != leaves2.size()) {
+            return false;
+        }
+
+        for (int i = 0; i < leaves1.size(); i++) {
+            if (leaves1.get(i) != leaves2.get(i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private void preOrder(TreeNode node, List<Integer> leaves) {
+        if (node == null) {
+            return;
+        }
+
+        if (node.left == null && node.right == null) {
+            leaves.add(node.val);
+        }
+        preOrder(node.left, leaves);
+        preOrder(node.right, leaves);
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
