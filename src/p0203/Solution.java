b27db35bb6994cbd34c4a36fa7b071af7364195f
zengzhirong87@163.com <zengzhirong87@163.com>
2021-06-06 10:13:19 +0800

    203

---
+++ src/p0203/Solution.java
@@ -0,0 +1,35 @@
+package p0203;
+
+public class Solution {
+
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode vh = new ListNode(0, head);
+        ListNode prev = vh;
+
+        while (prev.next != null) {
+            if (prev.next.val == val) {
+                prev.next = prev.next.next;
+            } else {
+                prev = prev.next;
+            }
+        }
+        return vh.next;
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
